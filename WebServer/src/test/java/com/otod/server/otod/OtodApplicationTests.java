0844382b223de569498573d0e3f2da33a33a851d
MinRam <997264157@qq.com>
2018-05-20 02:17:32 +0800

    Add Server

---
+++ WebServer/src/test/java/com/otod/server/otod/OtodApplicationTests.java
@@ -0,0 +1,16 @@
+package com.otod.server.otod;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OtodApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
