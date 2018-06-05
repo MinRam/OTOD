583dfe1d418a6377eed082d7712755029b9f6a4d
DogeZhang <zhangyz86@hotmail.com>
2018-06-05 09:36:43 +0800

    调整图标

--- WebClient/Vue_BS/otod/src/pages/Service.vue
+++ WebClient/Vue_BS/otod/src/pages/Service.vue
@@ -9,7 +9,7 @@
                             <el-col :span="24">
                                 <el-card shadow="hover">
                                     <div v-if="m.emergency === '1'" style="height: 3px; clear: both; width: 100%" class="bg-danger"></div>
-                                    <div v-if="m.emergency === '2'" style="height: 3px; clear: both; width: 100%" class="bg-warming"></div>
+                                    <div v-if="m.emergency === '2'" style="height: 3px; clear: both; width: 100%" class="bg-warning"></div>
                                     <div v-if="m.emergency === '3'" style="height: 3px; clear: both; width: 100%" class="bg-success"></div>
                                     <div v-if="m.emergency === '4'" style="height: 3px; clear: both; width: 100%" class="bg-info"></div>
                                     <el-container class="bg-purple">
@@ -19,7 +19,7 @@
                                                     <i class="el-icon-menu" style="font-size: 60px;color: #409EFF"></i>
                                                 </div>
                                                 <div class="user-info">
-                                                    <p>zhangyizhuo</p>
+                                                    <p>{{ m.user_name }}</p>
                                                     <!-- <p>Nothing</p> -->
                                                 </div>
                                                 <div style="clear: both"></div>
@@ -28,14 +28,16 @@
                                                 <div style="margin-bottom: 10px;">
                                                     <el-button type="success" icon="el-icon-check" circle @click="sstatic = !sstatic" @mouseover.native="show = !show" @mouseout.native="show = !show"></el-button>
                                                     <transition name="el-zoom-in-left">
-                                                        <div v-show="show || sstatic" class="commit-button-box"></div>
+                                                        <div v-show="show || sstatic" class="commit-button-box bg-success"></div>
                                                     </transition>
                                                     <div style="clear: both"></div>
                                                 </div>
                                                 <div style="margin-bottom: 10px;">
-                                                    <el-button type="success" icon="el-icon-check" circle @click="sstatic1 = !sstatic1" @mouseover.native="show1 = !show1" @mouseout.native="show1 = !show1"></el-button>
+                                                    <el-button type="warning" icon="el-icon-arrow-right" circle @click="sstatic1 = !sstatic1" @mouseover.native="show1 = !show1" @mouseout.native="show1 = !show1"></el-button>
                                                     <transition name="el-zoom-in-left">
-                                                        <div v-show="show1 || sstatic1" class="commit-button-box"></div>
+                                                        <div v-show="show1 || sstatic1" class="commit-button-box bg-warning">
+                                                            <span>我需要{{ m.contributers }}个人</span>
+                                                        </div>
                                                     </transition>
                                                     <div style="clear: both"></div>
                                                 </div>
@@ -67,19 +69,33 @@ export default {
       show1: false,
       message: [{
         title: '标题一',
+        user_name: 'zhangyz',
         content: 'Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !',
+        contributers: '4',
         emergency: '1'
       }, {
         title: '帮忙购买炒酸奶',
+        user_name: 'zhangyh',
         content: 'Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !',
+        contributers: '2',
         emergency: '2'
       }, {
         title: '帮忙拿快递，快递号131313131',
+        user_name: 'zhangyh',
         content: 'Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !',
+        contributers: '1',
         emergency: '3'
       }, {
         title: '我也不知道该写些什么就这样吧',
+        user_name: 'zhangyizhuo',
         content: 'Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !',
+        contributers: '5',
+        emergency: '4'
+      }, {
+        title: '我也不知道该写些什么就这样吧',
+        user_name: 'DogeZhang',
+        content: 'Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !Content !',
+        contributers: '6',
         emergency: '4'
       }]
     }
