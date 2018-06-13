c5aa4e21d51008b9c152f2f4a81e2d2eccd1a6f0
dfzz <dfzz_free@163.com>
2018-06-13 22:11:59 +0800

    file pages
    添加了文档管理的部分，css还无法加载

---
+++ WebClient/Vue_BS/otod/src/components/file/myfiles/myinfo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="myinfo"></div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style></style>
---
+++ WebClient/Vue_BS/otod/src/components/file/myfiles/myupload.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="myupload">
+    <ul id="list-ul">
+      <li v-for="file in filelist" :key="file.id">
+        <div class="card" style="width: 10rem;">
+          <div class="card-block card-float">
+            <h4 class="card-title">{{file.id}}</h4>
+            <p class="card-text">{{file.name}}</p>
+            <router-link class="card-link" :to="{path: '/files', query: {filename: JSON.stringify(file)}}" target="_blank">查看</router-link>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'myupload',
+  data () {
+    return {
+      filelist: [],
+      userid: 1
+    }
+  },
+  created () {
+    this.filelist = []
+    var url = 'http://127.0.0.1:8082/vrss/FileInfo/list'
+    var params = new URLSearchParams()
+    params.append('user_id', this.userid)
+    params.append('tag_id', 0)
+    params.append('key', null)
+    params.append('type', 9)
+    this.$http.post(url, params).then((response) => {
+      var data = response.data
+      console.log(data)
+      this.filelists = []
+      for (var i = 0; i < data.length; i++) {
+        this.filelist.push({
+          id: data[i].id,
+          name: data[i].name,
+          create_time: data[i].create_time,
+          description: data[i].description
+        })
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+  @import '../../../assets/bootstrap/css/bootstrap.css'
+  .card-float {
+    position: relative;;
+    float: left
+  }
+</style>
