da47618b829cd1f538f4a0d8749c7eebdcd6ff02
dfzz <dfzz_free@163.com>
2018-06-15 17:00:46 +0800

    file pages

--- WebClient/Vue_BS/otod/src/components/file/files/file.vue
+++ WebClient/Vue_BS/otod/src/components/file/files/file.vue
@@ -9,17 +9,14 @@
               {{ file.name }}<br>
               {{ file.description }}<br>
               <a v-bind:href="'http://127.0.0.1:8082/vrss/Download/download?file_url=' + file.file_url" v-bind:download="file.name">下载</a>
-              <span class="switcher" v-bind:class="{'left': isClose, 'right': isOpen}" @click="switcher()">
+              <span class="switcher" v-bind:class="{'left': isClose, 'right': !isClose}" @click="switcher()">
                 <p v-if="isClose == true">未收藏（点击收藏）</p>
                 <p v-else>
-                  已收藏至
+                  已收藏
                 </p>
               </span>
-              <select v-show="isOpen">
-                <option value ="0">默认收藏夹</option>
-                <option value ="1">收藏1</option>
-                <option value="2">收藏2</option>
-                <option value="3">收藏3</option>
+              <select v-model="myvalue">
+                <option v-for="item in optionlist" :key="item.id" :value="item.id">{{item.name}}</option>
               </select>
             </p>
           </div>
@@ -38,24 +35,73 @@ export default {
       file: {
         type: Object
       },
-      isClose: true,
-      isOpen: false// 假设默认关闭
+      isClose: true, // 假设默认未收藏
+      optionlist: [],
+      myvalue: 0 // 默认是0
     }
   },
   created () {
-    console.log(this.id)
-    var url = 'http://127.0.0.1:8082/vrss/FileInfo/get'
-    var params = new URLSearchParams()
-    params.append('file_id', this.id)
-    this.$http.post(url, params).then((response) => {
-      var data = response.data
-      console.log(data)
-      this.file = data
-    }).catch((error) => {
-      console.log(error)
-    })
+    this.getinfo()
+    this.getlist()
+  },
+  destroyed () {
+
   },
   methods: {
+    isLike () {
+      var url = 'http://127.0.0.1:8082/vrss/FileInfo/lovestate'
+      var params = new URLSearchParams()
+      params.append('user_id', this.userid)
+      params.append('file_id', this.id)
+      this.$http.post(url, params).then((response) => {
+        var data = response.data
+        console.log(data)
+        if (data === true) {
+          this.isClose = false
+        }
+      }).catch((error) => {
+        console.log(error)
+      })
+    },
+    getinfo () {
+      var url = 'http://127.0.0.1:8082/vrss/FileInfo/get'
+      var params = new URLSearchParams()
+      params.append('file_id', this.id)
+      this.$http.post(url, params).then((response) => {
+        var data = response.data
+        console.log(data)
+        this.file = data
+      }).catch((error) => {
+        console.log(error)
+      })
+    },
+    getlist () {
+      var url1 = 'http://127.0.0.1:8082/vrss/FileList/listfilelist'
+      var params1 = new URLSearchParams()
+      params1.append('user_id', this.userid)
+      params1.append('tag_id', 0)
+      params1.append('key', null)
+      params1.append('type', 7)
+      this.$http.post(url1, params1).then((response) => {
+        var data = response.data
+        console.log(data)
+        if (data != null) {
+          for (var i = 0; i < data.length; i++) {
+            this.optionlist.push({
+              id: data[i].id,
+              name: data[i].name
+            })
+          }
+        }
+      }).catch((error) => {
+        console.log(error)
+      }).then(() => {
+        this.optionlist.unshift({
+          id: 0,
+          name: '默认收藏夹'
+        })
+      })
+    },
     adddownload () {
       var url = 'http://127.0.0.1:8082/vrss/Download/add'
       var params = new URLSearchParams()
@@ -70,7 +116,6 @@ export default {
     switcher: function () {
       // s实现开关切换
       this.isClose = !this.isClose
-      this.isOpen = !this.isOpen
     }
   }
 }
